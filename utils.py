ec95164bd309b5c896caf2b3f68d005219a2de4e
Hacker hub 8 <103352759+hacker-hub8@users.noreply.github.com>
2025-01-08 13:54:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,17 +3,26 @@ import os
 def clear_screen():
     """Clears the terminal screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
-
+    
 def print_banner():
-    """Prints the tool's banner."""
-    print('''
-  ______                          ______             _     ___  _    
-.' ____ \                        |_   _ `.          (_)  .' ..]/ |_  
-| (___ \_| .---.  ,--.   _ .--.    | | `. \ _ .--.  __  _| |_ `| |-' 
- _.____`. / /'`\]`'_\ : [ `.-. |   | |  | |[ `/'`\][  |'-| |-' | |   
-| \____) || \__. // | |, | | | |  _| |_.' / | |     | |  | |   | |,  
- \______.''.___.'\--;__/[___||__]|______.' [___]   [___][___]  \__/  
-                                                                     
-[ ScanDrift - Advanced Port Scanner and Exploitation Framework ]
-Version: 2.0.0 | Author: HackerHub8
-    ''')
+    RED = '\033[91m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    BOLD = '\033[1m'
+    ENDC = '\033[0m'
+
+    print(RED + BOLD + '''
+      ______                          ______             _     ___  _    
+    .' ____ \                        |_   _ `.          (_)  .' ..]/ |_  
+    | (___ \_| .---.  ,--.   _ .--.    | | `. \ _ .--.  __  _| |_ `| |-' 
+     _.____`. / /'`\]`'_\ : [ `.-. |   | |  | |[ `/'`\][  |'-| |-' | |   
+    | \____) || \__. // | |, | | | |  _| |_.' / | |     | |  | |   | |,  
+     \______.''.___.'\--;__/[___||__]|______.' [___]   [___][___]  \__/  
+                                                                         
+     \t''' + BLUE + '[ Open Port Scanner & Exploiter upon Network by HackerHub8 ]' + ENDC + '''
+     \t\t
+    ''' + GREEN + '░░░░░░███████ ]▄▄▄▄▄▄▄▄' + ENDC + '''   \t''' + BLUE + 'Version' + ENDC + ''': 1.0.2
+    ''' + GREEN + ' ▂▄▅█████████▅▄▃▂      ' + ENDC + '''   \t''' + BLUE + 'Author' + ENDC + ''': HackerHub8
+    ''' + GREEN + '[███████████████████]. ' + ENDC + '''
+    ''' + GREEN + '◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤..   ' + ENDC + '''          
+    ''' + ENDC)
